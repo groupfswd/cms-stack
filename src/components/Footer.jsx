6cd1a17c26fdf667c3c46205d47db0115efddeae
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-05-29 01:20:01 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
     <div>
-      <h1>this is footer</h1>
+      <footer class="bg-gray-800 shadow w-full dark:bg-gray-800 bottom-0 absolute">
+          <div class="w-full mx-auto max-w-screen-xl p-6 md:flex md:items-center md:justify-center">
+            <span class="text-md text-white sm:text-center">© 2024 Stack<span style={{ color: "#2563EB" }}>Master</span>. All Rights Reserved.
+          </span>
+          </div>
+      </footer>
     </div>
   );
 }
